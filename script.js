e92b760892a8a42ea7518420a7b42cb3bf3902d0
adeniranadeyemo <niranadeniran1@gmail.com>
2025-01-23 16:41:12 +0100

    More regex!!

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const infixToFunction = {
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+  '/': (x, y) => x / y,
+};
+
+const infixEval = (str, regex) =>
+  str.replace(regex, (_match, arg1, operator, arg2) =>
+    infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
+  );
+
+const highPrecedence = (str) => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+
+  return str2 === str ? str : highPrecedence(str2);
+};
+
+// console.log(infixEval('5-3', /(\d+)([+*\/-])(\d+)/g));
+
 const isEven = (num) => num % 2 === 0;
 const sum = (nums) => nums.reduce((acc, curr) => acc + curr, 0);
 const average = (nums) => sum(nums) / nums.length;
@@ -18,6 +39,10 @@ const spreadsheetFunctions = {
   median,
 };
 
+const applyFunction = (str) => {
+  const noHigh = highPrecedence(str);
+};
+
 const range = (start, end) =>
   Array(end - start + 1)
     .fill(start)
@@ -27,6 +52,34 @@ const charRange = (start, end) =>
   range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
     String.fromCharCode(code)
   );
+
+const evalFormula = (x, cells) => {
+  const idToText = (id) => cells.find((cell) => cell.id === id).value;
+
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+
+  const elemValue = (num) => {
+    const inner = (character) => {
+      return idToText(character + num);
+    };
+    return inner;
+  };
+
+  const addCharacters = (character1) => (character2) => (num) =>
+    charRange(character1, character2).map(elemValue(num));
+
+  const rangeExpanded = x.replace(rangeRegex, (_, char1, num1, char2, num2) =>
+    rangeFromString(num1, num2).map(addCharacters(char1)(char2))
+  );
+
+  const cellRegex = /([A-J])([1-9][0-9]?)/gi;
+
+  const cellExpanded = rangeExpanded.replace(cellRegex, (match) =>
+    idToText(match.toUpperCase())
+  );
+};
 
 window.onload = () => {
   const container = document.getElementById('container');
